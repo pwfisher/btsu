0167e23afeae7131ae8af91489ecb4cefc94fd27
Patrick Fisher <patrick@pwfisher.com>
2013-03-15 21:00:27 -0700

    update parseArgs comment block

--- s3common.php
+++ s3common.php
@@ -156,8 +156,10 @@ function dieError($error,$code=1) {
 }
 
 /**
- * parseArgs by Patrick Fisher 
- * http://pwfisher.com/nucleus/index.php?itemid=45
+ * parseArgs Command Line Interface (CLI) utility function.
+ * @usage               $args = parseArgs($_SERVER['argv']);
+ * @author              Patrick Fisher <patrick@pwfisher.com>
+ * @source              https://github.com/pwfisher/CommandLine.php
  */
 function parseArgs($argv){
     array_shift($argv); $o = array();
